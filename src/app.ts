110ca56f6d9b80d290409d7466e9b2d2834c3057
Jose Szychowski <joseszychowski@gmail.com>
2024-02-18 16:07:07 -0300

    add env variables into github action

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,27 @@ const prisma = new PrismaClient();
 const app: Express = express();
 
 app.get('/add', async (req, res) => {
-    const createdData = await prisma.user.create({
-        data: {
-            name: 'jose',
-            email: `${Math.trunc(Math.random() * 999999)}`,
-            age: Math.trunc(Math.random() * 99),
-        },
-    });
-    res.status(200).json(createdData);
+    try {
+        const createdData = await prisma.user.create({
+            data: {
+                name: 'jose',
+                email: `${Math.trunc(Math.random() * 999999)}`,
+                age: Math.trunc(Math.random() * 99),
+            },
+        });
+        res.status(200).json(createdData);
+    } catch (error) {
+        res.status(500).json('Internal Server Error');
+    }
 });
 
 app.get('/show', async (req, res) => {
-    const createdData = await prisma.user.findMany();
-    res.status(200).json(createdData);
+    try {
+        const createdData = await prisma.user.findMany();
+        res.status(200).json(createdData);
+    } catch (error) {
+        res.status(500).json('Internal Server Error');
+    }
 });
 
 app.get('/command', async (req, res) => {
