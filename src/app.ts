e0b3192dc574a52f5b5a7899221bf29d2d5932ce
Jose Szychowski <joseszychowski@gmail.com>
2024-02-19 09:42:29 -0300

    install fastify for testing

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Express } from 'express';
 import { PrismaClient } from '@prisma/client';
+import Fastify from 'fastify';
 
 const prisma = new PrismaClient();
 
+// Express app
 const app: Express = express();
 
 app.get('/add', async (req, res) => {
@@ -36,3 +39,27 @@ app.get('/command', async (req, res) => {
 app.listen(3000, () => {
     console.log('App listening on port 3000...');
 });
+
+// Fastify app
+const fastify = Fastify();
+fastify.get('/fastAdd', async function handler(request, reply) {
+    try {
+        const createdData = await prisma.user.create({
+            data: {
+                name: 'jose',
+                email: `${Math.trunc(Math.random() * 999999)}`,
+                age: Math.trunc(Math.random() * 99),
+            },
+        });
+        return createdData;
+    } catch (error) {
+        return { error: 'Internal Server Error' };
+    }
+});
+
+try {
+    fastify.listen({ port: 3001 });
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
